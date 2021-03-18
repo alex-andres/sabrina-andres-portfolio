e44df3c5bc0ea83797f86aa3010f0d0c2e4ea525
c-andres <charlesalexanderandres@gmail.com>
2021-03-17 23:48:02 -0700

    testing reset functionality on react form round 2

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 import { useForm } from "react-hook-form";
 import SubmitButton from "./SubmitButton";
@@ -15,28 +15,24 @@ import { motion } from "framer-motion";
 
 const ContactForm = ({ className }) => {
   const { register, handleSubmit, reset, errors } = useForm({ mode: "onBlur" });
-  const [messageSent, setMessageSent] = useState('initial');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [message, setMessage] = useState('');
+  // const [messageSent, setMessageSent] = useState('initial');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
   const netlify = useNetlifyForm({
     name: "ReCAPTCHA",
-    action: "/thanks",
+    action: "./thank-you",
     honeypotName: "bot-field",
     onSuccess: (response, context) => {
-      console.log('Successfully sent form data to Netlify Server')
-      context.formRef.current.reset()
+      console.log("Successfully sent form data to Netlify Server");
+      context.formRef.current.reset();
     },
   });
   const onSubmit = (data) => {
-    netlify.handleSubmit(null, data).then(()=>{
-      if(netlify.success){
-        setMessageSent('sent');
-      // } else if(netlify.error){
-      //   setMessageSent('error');
-      }
+    netlify.handleSubmit(null, data).then(() => {
+      console.log("success");
     });
   };
   const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i;
@@ -52,122 +48,133 @@ const ContactForm = ({ className }) => {
       },
     },
   };
-  if(messageSent === 'initial'){
-    return (
-      <ContactContainer
-        initial="hidden"
-        animate="visible"
-        variants={slideUpDelay2}
-        className={className}
-      >
-        <NetlifyFormProvider className="netlify-form-provider" {...netlify}>
-          <NetlifyFormComponent onSubmit={handleSubmit(onSubmit)} id="form-component">
-            <Honeypot />
-            <Recaptcha siteKey={process.env.GATSBY_SITE_RECAPTCHA_KEY} invisible />
-            {netlify.success && <p>Thanks for contacting us!</p>}
-            {netlify.error && <p>Sorry, we could not reach servers.</p>}
-            <div className="flex-container">
-              <div className="input-wrapper">
-                <label className="label" htmlFor="firstNameMessage">
-                  First Name:
-                </label>
-                <Input
-                  style={{ border: errors.firstNameMessage && "red 2px solid" }}
-                  type="text"
-                  name="firstNameMessage"
-                  id="firstNameMessage"
-                  ref={register({
-                    required: "First Name is Required",
-                    maxLength: 80,
-                  })}
-                  onChange={(e)=>setFirstName(e.target.value)}
-                  value={firstName}
-                />
+  return (
+    <ContactContainer
+      initial="hidden"
+      animate="visible"
+      variants={slideUpDelay2}
+      className={className}
+    >
+      <NetlifyFormProvider className="netlify-form-provider" {...netlify}>
+        <NetlifyFormComponent
+          onSubmit={handleSubmit(onSubmit)}
+          id="form-component"
+        >
+          <Honeypot />
+          <Recaptcha
+            siteKey={process.env.GATSBY_SITE_RECAPTCHA_KEY}
+            invisible
+          />
+          {netlify.success && <p>Thanks for contacting us!</p>}
+          {netlify.error && <p>Sorry, we could not reach servers.</p>}
+          <div className="flex-container">
+            <div className="input-wrapper">
+              <label className="label" htmlFor="firstNameMessage">
+                First Name:
+              </label>
+              <Input
+                style={{ border: errors.firstNameMessage && "red 2px solid" }}
+                type="text"
+                name="firstNameMessage"
+                id="firstNameMessage"
+                ref={register({
+                  required: "First Name is Required",
+                  maxLength: 80,
+                })}
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+              />
+            </div>
+            {errors.firstNameMessage && (
+              <div className="errors-message">
+                {errors.firstNameMessage.message}
               </div>
-              {errors.firstNameMessage && <div className="errors-message">{errors.firstNameMessage.message}</div>}
-              <div className="input-wrapper">
-                <label className="label" htmlFor="lastNameMessage">
-                  Last Name:
-                </label>
-                <Input
-                  style={{ border: errors.lastNameMessage && "red 2px solid" }}
-                  type="text"
-                  name="lastNameMessage"
-                  id="lastNameMessage"
-                  ref={register({
-                    required: "Last Name is Required",
-                    maxLength: 100,
-                  })}
-                  onChange={(e)=>setLastName(e.target.value)}
-                  value={lastName}
-                />
+            )}
+            <div className="input-wrapper">
+              <label className="label" htmlFor="lastNameMessage">
+                Last Name:
+              </label>
+              <Input
+                style={{ border: errors.lastNameMessage && "red 2px solid" }}
+                type="text"
+                name="lastNameMessage"
+                id="lastNameMessage"
+                ref={register({
+                  required: "Last Name is Required",
+                  maxLength: 100,
+                })}
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+              />
+            </div>
+            {errors.lastNameMessage && (
+              <div className="errors-message">
+                {errors.lastNameMessage.message}
               </div>
-              {errors.lastNameMessage && <div className="errors-message">{errors.lastNameMessage.message}</div>}
-              <div className="input-wrapper">
-                <label className="label" htmlFor="email">
-                  Email:
-                </label>
-                <Input
-                  style={{ border: errors.email && "red 2px solid" }}
-                  type="email"
-                  name="email"
-                  id="email"
-                  ref={register({
-                    required: "Email is required",
-                    pattern: {
-                      value: EMAIL_REGEX,
-                      message: "Invalid email address",
-                    },
-                  })}
-                  onChange={(e)=>setEmail(e.target.value)}
-                  value={email}
-                />
-              </div>
-              {errors.email && <div className="errors-message">{errors.email.message}</div>}
-              <div className="input-wrapper">
-                <label className="label" htmlFor="phone">
-                  Phone:
-                </label>
-                <Input
+            )}
+            <div className="input-wrapper">
+              <label className="label" htmlFor="email">
+                Email:
+              </label>
+              <Input
+                style={{ border: errors.email && "red 2px solid" }}
+                type="email"
+                name="email"
+                id="email"
+                ref={register({
+                  required: "Email is required",
+                  pattern: {
+                    value: EMAIL_REGEX,
+                    message: "Invalid email address",
+                  },
+                })}
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+            </div>
+            {errors.email && (
+              <div className="errors-message">{errors.email.message}</div>
+            )}
+            <div className="input-wrapper">
+              <label className="label" htmlFor="phone">
+                Phone:
+              </label>
+              <Input
                 style={{ border: errors.phone && "red 2px solid" }}
-                  type="tel"
-                  name="phone"
-                  ref={register({ required: "Phone is Required", maxLength: 12 })}
-                  onChange={(e)=>setPhone(e.target.value)}
-                  value={phone}
-                />
-              </div>
-              {errors.phone && <div className="errors-message">Phone is Required</div>}
-              <div className="input-wrapper message-input-wrapper">
-                <label className="label" htmlFor="Message">
-                  Message:
-                </label>
-                <textarea
-                  style={{ border: errors.Message && "red 2px solid" }}
-                  name="Message"
-                  ref={register({ required: "Message is Required" })}
-                  onChange={(e)=>setMessage(e.target.value)}
-                  value={message}
-                />
-              </div>
-              {errors.Message && <div className="errors-message">{errors.Message.message}</div>}
+                type="tel"
+                name="phone"
+                ref={register({ required: "Phone is Required", maxLength: 12 })}
+                onChange={(e) => setPhone(e.target.value)}
+                value={phone}
+              />
             </div>
-  
-            <div className="button-container">
-              <SubmitButton type="submit">Send</SubmitButton>
+            {errors.phone && (
+              <div className="errors-message">Phone is Required</div>
+            )}
+            <div className="input-wrapper message-input-wrapper">
+              <label className="label" htmlFor="Message">
+                Message:
+              </label>
+              <textarea
+                style={{ border: errors.Message && "red 2px solid" }}
+                name="Message"
+                ref={register({ required: "Message is Required" })}
+                onChange={(e) => setMessage(e.target.value)}
+                value={message}
+              />
             </div>
-            <button
-            type='reset'
-            onClick={() => reset()}
-          >
-            Reset
-          </button>
-          </NetlifyFormComponent>
-        </NetlifyFormProvider>
-      </ContactContainer>
-    );
-  };
-  
+            {errors.Message && (
+              <div className="errors-message">{errors.Message.message}</div>
+            )}
+          </div>
+
+          <div className="button-container">
+            <SubmitButton type="submit">Send</SubmitButton>
+          </div>
+        </NetlifyFormComponent>
+      </NetlifyFormProvider>
+    </ContactContainer>
+  );
 };
 
 export default ContactForm;
