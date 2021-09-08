904df4d361959a41fa03796f679dfb3b7ea67e5a
c-andres <charlesalexanderandres@gmail.com>
2021-09-08 13:06:49 -0700

    add further styling to thank-you.js

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -15,13 +15,18 @@ const StyledContactConfirmation = styled.div`
   align-items: center;
 
   justify-content: center;
+  height: 100vh;
   width: 100%;
-  margin: 8rem auto;
+  margin: -8rem auto -1rem;
   padding: 0 3vw;
   h1{
     margin-bottom: 1rem;
   }
   p{
     margin-bottom: 4rem;
+  }
+  @media screen and (min-width: 769px){
+    height: 80vh;
+    margin: -8rem auto 0;
   }
 `
